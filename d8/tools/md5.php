9a36705fa3519390e3ca7f59f80028a072a738f7
foxzld <yu876696862@foxmail.com>
2013-12-02 14:24:34 +0800

    md5加密功能实现

--- d8/tools/md5.php
+++ d8/tools/md5.php
@@ -1,31 +1,47 @@
 
 
-<script language="JavaScript">
-function CalcuMD5()
-{   
-  var oText = document.getElementById("oText");
-  jsMD5_Typ = 'Typ16';
-  var aCode = MD5(oText);
-  document.aCode.value = aCode;
-  document.aCod2.value = aCode.toLowerCase();
-  jsMD5_Typ = 'Typ32';
-  var bCode = MD5(oText);
-  document.bCode.value = bCode;
-  document.bCod2.value = bCode.toLowerCase();
-                        
-}
-</script>
-<h2 class="sitetip">请输入要加密的字符串:</h2>
+<h2 class="sitetip">md5加密工具,将字符串md5加密</h2>
 
-<textarea onmouseover="this.focus();this.select();" class="tarea" name="oText" id="oText"></textarea>
-
-<div class="subbtn">
-
-	<input type="button" onClick="CalcuMD5()" value="开始转换" class="btn btn-primary">
-    
-</div>
-
-<h2 class="sitetip">相应的HTML代码: 16位</h2>
-
-<input name="aCode" type="text" id="aCode"  size="70"></input>
-<script language="JavaScript" src="<?php bloginfo('template_url') ?>/tools/jsMD5.js"></script>
+ <form action="" method="post" name="fmCalcu" id="fmCalcu">
+    <tr align="center"> 
+      <td colspan="2" class="t15"><b>请输入要加密的字符串</b></td>
+    </tr>
+    <tr> 
+      <td width="448"> 
+        <textarea name="oText" cols="70" rows="7" class="tarea" id="oText"></textarea>
+      </td>
+      <td width="136">
+        <input type="button" name="Button" value=" 原文字字串加密 " onClick="CalcuMD5()" class="btn btn-primary" />
+      </td>
+    </tr><br/><br/><br/>
+    <tr>
+      <td width="136" align="center">16位(大)
+      </td>	
+      <td width="448"> 
+        <input name="aCode" type="text" id="aCode" value="" size="70" />
+      </td>
+      
+    </tr><br />
+    <tr> 
+	<td width="136">16位(小)
+      </td>
+      <td width="448"> 
+        <input name="aCod2" type="text" id="aCod2" value="" size="70" />
+      </td>
+      
+    </tr><br />
+    <tr> 
+	<td width="136">32位(大)</td>
+      <td width="448"> 
+        <input name="bCode" type="text" id="bCode" value="" size="70" />
+      </td>
+      
+    </tr><br />
+    <tr> 
+	<td width="136">32位(小)</td>
+      <td width="448"> 
+        <input name="bCod2" type="text" id="bCod2" value="" size="70" />
+      </td>
+      
+    </tr>
+  </form>
