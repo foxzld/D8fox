ade8eaf6dd3b375395636c6ff3df52fec014c3b5
foxzld <yu876696862@foxmail.com>
2013-12-03 15:27:19 +0800

    修改404页面

--- d8/404.php
+++ d8/404.php
@@ -1,18 +1,111 @@
 <?php get_header(); ?>
 <div class="content-wrap">
-	<div style="text-align:center;padding:60px 0;font-size:16px;">
-		<h2 style="font-size:60px;margin-bottom:32px;">404 . Not Just An Error</h2>
-		抱歉，沒有找到您需要的内容！！
-	</div>
-	<h1 class="page-title">给您推荐以下内容：</h1>
-	<?php 
-		$args = array(
-		    'order'   => DESC,
-		    'caller_get_posts' => 1,
-		    'posts_per_page' => 4
-		);
-   		query_posts($args);
-	?>
-	<?php include( 'modules/excerpt.php' ); ?>
+	<style>
+            
+           
+            .wrap_404{margin:0 auto;padding:0 54px 35px;width: 392px;background: #fff;box-shadow: 0 0 5px rgba(0, 0, 0, 0.2);}
+            .wrap_404 .txt{height:180px;font-size:140px;font-family:'Arial';color: #4f46a0;text-align:center;line-height:1.4;}
+            .wrap_404 .purple{color: #4f46a0;text-align: center;font-size:17px;}
+
+            .wrap_404 .hr{margin:10px 0 20px;color: #666;font-size:17px;overflow: hidden;}
+            .wrap_404 .hr .line{float: left;width:40px;height:1px;background:#666;margin-top:10px;}
+            .wrap_404 .hr span{margin:0 10px;float:left;}
+            .wrap_404 .remind_list{overflow: hidden;margin-bottom:20px;}
+            .wrap_404 li{margin:0 55px 20px 0;float:left;width:90px;}
+            .wrap_404 li.last{margin-right:0;}
+            .wrap_404 li .img{width:90px;height:90px;background:url(<?php bloginfo('template_directory'); ?>/img/404.png) 0 -302px no-repeat;}
+            .wrap_404 li .img span{width:90px;height:90px;display: block;background:url(<?php bloginfo('template_directory'); ?>/img/404.png) no-repeat;}
+            .wrap_404 li p{text-align: center;margin-top:5px;color: #666;}
+            .wrap_404 li .img span.miss1{background-position:0px -420px;}
+            .wrap_404 li .img span.miss2{background-position:-148px -420px;}
+            .wrap_404 li .img span.miss3{background-position:-297px -420px;}
+            .wrap_404 li .img span.miss4{background-position:0px -561px;}
+            .wrap_404 li .img span.miss5{background-position:-150px -561px;}
+            .wrap_404 li .img span.miss6{background-position:-297px -561px;}
+            .wrap_404 li .img span.miss7{background-position:-295px -302px}
+            .wrap_404 li .img span.miss8{background-position:0px -699px}
+            .wrap_404 li .img span.miss9{background-position:-148px -699px}
+            .wrap_404 li .img span.miss0{background-position:-300px -699px}
+            .wrap_404 .link_404 a{color: #666;}
+            .wrap_404 .link_404 a:hover{color: #4f46a0;}
+            .wrap_404 .link_404 span{color: #666;margin:0 15px;}
+            .footer_404{text-align: center;color: #666;margin:20px 0 60px;}
+            .footer_404 a:hover{color: #4f46a0;}
+        </style>       
+		<div class="fuck404">
+			<!-- 丁香园404 start-->
+	<div class="wrap_404">
+            <div class="txt">404</div>
+            <p class="purple">你所访问的页面就如那些遇害的同道，已离我们远去</p>
+            <div class="hr">
+                <div class="line"></div>
+                <span>让我们记住这些遇害同道的名字和面容</span>
+                <div class="line"></div>
+            </div>
+            <ul class="remind_list">
+                <li>
+                    <div class="img">
+                        <span class="miss8"></span>
+                    </div>
+                    <p>王云杰</p>
+                </li>
+                <li>
+                    <div class="img">
+                        <span class="miss7"></span>
+                    </div>
+                    <p>康红千</p>
+                </li>
+                <li class="last">
+                    <div class="img">
+                        <span class="miss5"></span>
+                    </div>
+                    <p>朱玉飞</p>
+                </li>
+                <li>
+                    <div class="img">
+                        <span class="miss1"></span>
+                    </div>
+                    <p>戴光琼</p>
+                </li>
+                <li>
+                    <div class="img">
+                        <span class="miss2"></span>
+                    </div>
+                    <p>王浩</p>
+                </li>
+                <li class="last">
+                    <div class="img">
+                        <span class="miss3"></span>
+                    </div>
+                    <p>续广军</p>
+                </li>
+                <li>
+                    <div class="img">
+                        <span class="miss4"></span>
+                    </div>
+                    <p>彭玲云</p>
+                </li>
+                <li>
+                    <div class="img">
+                        <span class="miss9"></span>
+                    </div>
+                    <p>戴春福</p>
+                </li>
+                <li class="last">
+                    <div class="img">
+                        <span class="miss0"></span>
+                    </div>
+                    <p>悲剧不再发生...</p>
+                </li>
+            </ul>
+            <p class="link_404">
+                <a href="<?php bloginfo('url'); ?>" target="_blank">博客首页</a>
+                <span>-</span>
+                <a href="http://search.dxy.cn" target="_blank">丁香搜索</a>
+                <span>-</span>
+                <a href="http://6d.dxy.cn/article/20477" target="_blank">中国大陆近年恶性医患冲突案例简编</a>
+            </p>
+        </div>
+		<!-- 丁香园404 end-->
 </div>
 <?php get_footer(); ?>
